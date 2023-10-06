8aff6836aa402422bced633ee216888b55ffa068
scyanh <scyanh@gmail.com>
2023-10-06 11:17:40 -0600

    clean code

--- pkg/domain/usecases/crawler_test.go
+++ pkg/domain/usecases/crawler_test.go
@@ -9,6 +9,38 @@ import (
 	"testing"
 )
 
+func TestCrawler_Crawl(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// Load test data
+	htmlHome, htmlAbout, htmlExpertise := loadTestData(t)
+
+	// Setup Mocks
+	repoMock := interfaces.NewMockIMemoryLinkRepository(ctrl)
+	httpClientMock := interfaces.NewMockIHTTPClient(ctrl)
+
+	setupMocksForURL(repoMock, "https://parserdigital.com")
+	setupMocksForURL(repoMock, "https://parserdigital.com/about/")
+	setupMocksForURL(repoMock, "https://parserdigital.com/expertise/")
+
+	httpClientMock.EXPECT().Get("https://parserdigital.com").Return(htmlHome, nil).AnyTimes()
+	httpClientMock.EXPECT().Get("https://parserdigital.com/about/").Return(htmlAbout, nil).AnyTimes()
+	httpClientMock.EXPECT().Get("https://parserdigital.com/expertise/").Return(htmlExpertise, nil).AnyTimes()
+
+	// Initialize the crawler
+	numWorkers := 4
+	crawler := NewCrawler(repoMock, httpClientMock, numWorkers)
+
+	// Execute the crawler
+	startLink := entities.Link{URL: "https://parserdigital.com"}
+	crawler.Crawl(startLink)
+
+	firstVisit := repoMock.IsFirstVisit(startLink.URL)
+	assert.False(t, firstVisit, "The URL should have been visited before.")
+
+}
+
 func getHTMLFromFile(filename string) (string, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -43,36 +75,4 @@ func setupMocksForURL(repoMock *interfaces.MockIMemoryLinkRepository, url string
 		}
 		return false
 	}).AnyTimes()
-}
-
-func TestCrawler_Crawl(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	// Load test data
-	htmlHome, htmlAbout, htmlExpertise := loadTestData(t)
-
-	// Setup Mocks
-	repoMock := interfaces.NewMockIMemoryLinkRepository(ctrl)
-	httpClientMock := interfaces.NewMockIHTTPClient(ctrl)
-
-	setupMocksForURL(repoMock, "https://parserdigital.com")
-	setupMocksForURL(repoMock, "https://parserdigital.com/about/")
-	setupMocksForURL(repoMock, "https://parserdigital.com/expertise/")
-
-	httpClientMock.EXPECT().Get("https://parserdigital.com").Return(htmlHome, nil).AnyTimes()
-	httpClientMock.EXPECT().Get("https://parserdigital.com/about/").Return(htmlAbout, nil).AnyTimes()
-	httpClientMock.EXPECT().Get("https://parserdigital.com/expertise/").Return(htmlExpertise, nil).AnyTimes()
-
-	// Initialize the crawler
-	numWorkers := 4
-	crawler := NewCrawler(repoMock, httpClientMock, numWorkers)
-
-	// Execute the crawler
-	startLink := entities.Link{URL: "https://parserdigital.com"}
-	crawler.Crawl(startLink)
-
-	firstVisit := repoMock.IsFirstVisit(startLink.URL)
-	assert.False(t, firstVisit, "The URL should have been visited before.")
-
 }
--- pkg/domain/usecases/worker_test.go
+++ pkg/domain/usecases/worker_test.go
@@ -1,1 +1,66 @@
 package usecases
+
+import (
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/scyanh/crawler/pkg/domain/entities"
+	"github.com/scyanh/crawler/pkg/domain/interfaces"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+)
+
+func TestWorker_Work(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// Setup Mocks
+	repoMock := interfaces.NewMockIMemoryLinkRepository(ctrl)
+	httpClientMock := interfaces.NewMockIHTTPClient(ctrl)
+
+	httpClientMock.EXPECT().Get("https://parserdigital.com").Return("", errors.New("error")).AnyTimes()
+	worker := &Worker{Repo: repoMock, HTTPClient: httpClientMock}
+
+	setupMocksForURL(repoMock, "https://parserdigital.com")
+
+	// Initialize channels and wait groups
+	toVisitChan := make(chan string, 1000)
+	visitedChan := make(chan entities.Link)
+	wgWorkers := &sync.WaitGroup{}
+	wgURLs := &sync.WaitGroup{}
+
+	wgWorkers.Add(1)
+	wgURLs.Add(1)
+	go worker.Work(1, wgWorkers, wgURLs, toVisitChan, visitedChan)
+
+	toVisitChan <- "https://parserdigital.com"
+	close(toVisitChan)
+
+	visitedLink := <-visitedChan
+
+	assert.Equal(t, "https://parserdigital.com", visitedLink.URL)
+	assert.True(t, visitedLink.Error)
+}
+
+func TestWorker_getLinks_ErrorFetching(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// Setup Mocks
+	repoMock := interfaces.NewMockIMemoryLinkRepository(ctrl)
+	httpClientMock := interfaces.NewMockIHTTPClient(ctrl)
+
+	httpClientMock.EXPECT().Get("https://parserdigital.com").Return("", errors.New("error")).AnyTimes()
+
+	worker := &Worker{Repo: repoMock, HTTPClient: httpClientMock}
+	links, err := worker.getLinks(1, "https://parserdigital.com")
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, len(links))
+}
+
+func TestWorker_isValidLink(t *testing.T) {
+	worker := &Worker{}
+	assert.True(t, worker.isValidLink("https://parserdigital.com/link1"))
+	assert.False(t, worker.isValidLink("https://notparserdigital.com/link1"))
+}
